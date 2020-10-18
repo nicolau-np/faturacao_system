067923d1493ca0c7265bf7c9877a5bb844a49952
Nicolau NP <mr1Normaliii@gmail.com>
2020-10-18 08:55:53 +0100

    preparacao do template parte 3

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layote');
-});
 
 Route::get('/login', 'UserController@loginForm')->name('login');
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
