59ce52c902bf73f69cb5b997bd58c4a4b212ac2c
Nicolau NP <mr1Normaliii@gmail.com>
2020-10-26 11:08:03 +0000

    reutilizacao do codigo criar produto

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -29,60 +29,14 @@ class CarrinhoController extends Controller
         return view("carrinho.list", $data);
     }
 
-    public function store(Request $request, $id)
+    public function store(Request $request, $id_notaVenda)
     {
-        $nota_venda = NotaVenda::find($id);
-        if (!$nota_venda) {
-            return back()->with(['error' => "Não encontrou a nota de venda"]);
-        }
-
-        if ($nota_venda->status == "terminado") {
-            return back()->with(['error' => "Já finalizou esta venda"]);
-        }
-
-        $totalProduto_processo = 0;
-        $produto = Produto::find($request->id_produto);
-
         $request->validate([
             'quantidade' => ['required', 'Integer'],
             'id_produto' => ['required', 'Integer']
         ]);
-        $data['where_itemVenda'] = [
-            'status' => "processo",
-            'id_produto' => $request->id_produto
-        ];
-        $data['item_venda'] = [
-            'id_nota_venda' => $id,
-            'id_usuario' => Auth::user()->id,
-            'id_produto' => $request->id_produto,
-            'quantidade' => $request->quantidade,
-            'valor_compra' => $produto->item_compra->last()->valor_compra,
-            'valor_venda' => $produto->item_compra->last()->valor_venda,
-            'valor' => ($produto->item_compra->last()->valor_venda * $request->quantidade)
-        ];
-
-        if (ItemVenda::where(['id_nota_venda' => $id, 'id_produto' => $request->id_produto])->first()) {
-            return back()->with(['error' => "Já adicionou este produto no carrinho"]);
-        }
-
-        if ($produto->quantidade < $request->quantidade) {
-            return back()->with(['error' => "Quantidade Indisponivel"]);
-        }
-
-        $item_venda = ItemVenda::getTotalproduto($data['where_itemVenda']);
-        if ($item_venda->count() >= 1) {
-            foreach ($item_venda as $item) {
-                $totalProduto_processo = $totalProduto_processo + $item->quantidade;
-            }
-        }
-        $total_produtoDisponivel = $produto->quantidade - $totalProduto_processo;
-        if ($total_produtoDisponivel < $request->quantidade) {
-            return back()->with(['error' => "Quantidade Indisponivel"]);
-        }
-
-        if (ItemVenda::create($data['item_venda'])) {
-            return back()->with(['success' => "Adicionado"]);
-        }
+        
+        return $this->criar_produto($id_notaVenda, $request->quantidade, $request->id_produto);
     }
 
     public function update(Request $request, $id_item_venda)
